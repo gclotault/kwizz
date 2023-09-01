a9720db2b484419c1b46e71314f8120227215dc8
gclotault <g.clotault@gmail.com>
2023-09-01 11:55:18 +0200

    Use Neon dialect instead of Vercel

--- database/config.ts
+++ database/config.ts
@@ -1,4 +1,24 @@
-import {createKysely} from "@vercel/postgres-kysely";
+import {NeonDialect} from "kysely-neon";
+import {Kysely} from "kysely";
+import {neonConfig} from "@neondatabase/serverless";
 import Database from "@/database/models/Database";
 
-export const db = createKysely<Database>();
+// if we're running locally
+if (process.env.NODE_ENV !== "production") {
+    // Set the WebSocket proxy to work with the local instance
+    neonConfig.wsProxy = (host) => `${host}:5433/v1`;
+    // Disable all authentication and encryption
+    neonConfig.useSecureWebSocket = false;
+    neonConfig.pipelineTLS = false;
+    neonConfig.pipelineConnect = false;
+}
+
+if (!process.env.POSTGRES_URL) {
+    throw new Error("POSTGRES_URL is not set");
+}
+
+export const db = new Kysely<Database>({
+    dialect: new NeonDialect({
+        connectionString: process.env.POSTGRES_URL,
+    }),
+});
