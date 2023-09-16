dd5b04a1bec4573c96dfdb42449e14ba23aec5c1
gclotault <g.clotault@gmail.com>
2023-09-16 18:28:02 +0200

    Updated migrations

--- prisma/migrations/20230913185636_theme_model/migration.sql
+++ prisma/migrations/20230913185636_theme_model/migration.sql
@@ -2,8 +2,10 @@
 CREATE TABLE "Theme" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL;
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Theme_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Theme_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 );
